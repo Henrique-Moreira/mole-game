59cb20df2a538dd37ba83367e75a8802713b0914
Henrique-Moreira <henriquemoreiraa@gmail.com>
2021-05-06 18:16:26 -0300

    cadastro concluido

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,12 +1,21 @@
-$(document).ready(function() {
-    $("#btnCadastro").click(function() {
+$(document).ready(function () {
+    $("#btnCadastro").click(function () {
         let $user = $("#user").val();
         let $pwd = $("#pwd").val();
-        let data = {"user": $user, "pwd": $pwd};
+        let data = { "user": $user, "pwd": $pwd };
+        let url = "http://localhost:8080/";
 
-        if($user && $pwd) {
-            let url = "http://localhost:8080/"
-            axios.post(url, data);
+        if ($user && $pwd) {
+            $.getJSON("http://localhost:8080/usuarios",
+                function ($registros) {
+                    if ($registros.find($usuario => $usuario.user == $user))
+                        alert("O usuário informado já tem cadastro, crie um usuário diferente.")
+                    else {
+                        axios.post(url, data);
+                        alert(`Usuário ${$user} criado com sucesso!`)
+                        window.open("login.html", "_self");
+                    }
+                });
         } else {
             alert("Erro: favor informar usuário e senha")
         }
--- js/login.js
+++ js/login.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-    $("#btnLogin").click(function() {
+$(document).ready(function () {
+    $("#btnLogin").click(function () {
         let $user = $("#user").val();
         let $pwd = $("#pwd").val();
-        if($user && $pwd) {
+        if ($user && $pwd) {
             $.getJSON("http://localhost:8080/usuarios",
-             function($registros) {
-               if ($registros.filter($usuario => $usuario.user == $user && $usuario.pwd == $pwd).length > 0)
-                        window.open("game.html", "_self")
-                else alert("Usuário Inválido");
-               });
+                function ($registros) {
+                    if ($registros.filter($usuario => $usuario.user == $user && $usuario.pwd == $pwd).length > 0)
+                        window.open("game.html", "_self");
+                    else {
+                        alert("Usuário Inválido");
+                    }
+                });
         } else {
             alert("Erro: favor informar usuário e senha")
         }
